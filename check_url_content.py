4c3d48d61599b6107e940607d24f0dfb50b07d41
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2025-11-16 19:18:08 +0100

    expanded url content scraping

--- check_url_content.py
+++ check_url_content.py
@@ -14,14 +14,48 @@ def scrape_content(url):
         for tag in soup(['script', 'style', 'footer', 'header', 'nav', 'aside']):
             tag.decompose()
 
-        # Tags that usually contain meaningful text
+        # Comprehensive list of HTML tags that can contain meaningful text
         meaningful_tags = [
+            # Headings
             'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-            'p', 'li',
-            'div', 'span', 'a',
-            'strong', 'em', 'b', 'i',
-            'article', 'section', 'main'
+    
+            # Paragraphs and lists
+            'p', 'li', 'ul', 'ol', 'dl', 'dt', 'dd',
+    
+            # Sections and structural
+            'div', 'span', 'article', 'section', 'main', 'header', 'footer', 'nav', 'aside',
+    
+            # Emphasis and inline formatting
+            'strong', 'em', 'b', 'i', 'mark', 'small', 'sub', 'sup', 'u', 'del', 'ins', 's', 'q', 'cite', 'code', 'pre', 'time',
+    
+            # Links
+            'a', 'link', 
+    
+            # Tables
+            'table', 'thead', 'tbody', 'tfoot', 'tr', 'td', 'th', 'caption',
+    
+            # Media captions
+            'figure', 'figcaption',
+    
+            # Forms
+            'form', 'label', 'button', 'option', 'select', 'textarea', 'input',
+    
+            # Quotes
+            'blockquote', 'q', 'cite',
+    
+            # Details / summary
+            'details', 'summary',
+    
+            # Address / contact
+            'address',
+    
+            # Miscellaneous / semantic
+            'abbr', 'data', 'time', 'var', 'samp', 'kbd', 'legend', 'fieldset',
+    
+            # Text containers
+            'bdi', 'bdo', 'ruby', 'rt', 'rp', 'wbr'
         ]
+
 
         # Extract text
         elements = soup.find_all(meaningful_tags)
