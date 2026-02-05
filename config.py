7ec07c31684b57d651a17f37f02e1e4dd0de7e37
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2026-02-05 14:02:59 +0100

    Clean up config.py by removing comments
    Removed comments and unnecessary blank lines from config.py.

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
 
-
-# Allowed extensions of the files to upload
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx', 'doc', 'jpg', 'jpeg', 'png', 'tiff', 'gif', 'bmp'}
-
-# Model settings
 MODEL_NAME = "roberta-base-openai-detector" 
